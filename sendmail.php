3ea705f7e3e1af20b6ce720036db3fa787f9e744
vigneshraj <94981768+lovelyvignesh@users.noreply.github.com>
2023-02-14 13:58:25 +0530

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,17 @@
+<!-- <?php
+if (isset($_POST['submit'])) {
+  $to = "user1@example.com, user2@example.com, user3@example.com";
+  $subject = "Important message";
+  $message = "This is a message for all users.";
+  $headers = "From: sender@example.com" . "\r\n";
+
+  if (mail($to, $subject, $message, $headers)) {
+    echo "Emails sent successfully.";
+  } else {
+    echo "An error occurred while sending the emails.";
+  }
+}
+?>
+<form action="" method="post">
+  <input type="submit" name="submit" value="Post">
+</form> -->
